a88652dc45ba812b3da625e923ccf547dd4acc06
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2023-05-13 20:28:14 +0100

    added all sections

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,8 @@ export default function Home() {
       <section className='flex items-center justify-center flex-col border border-gray-300 w-full md:p-14'>
 
         <div className='border border-yellow-500 w-full md:w-2/4 h-48  flex items-start flex-col'>
-          <div className='border border-gray-300 tracking-wide mb-6'>
-            personal digital healthcare company for everyone
+          <div className='border border-gray-300 tracking-wide mb-6 text-3xl'>
+              The #1 personal digital healthcare company for everyone
           </div>
 
           <div className='border border-gray-300 tracking-wide'>
@@ -29,7 +29,7 @@ export default function Home() {
 
         </div>
 
-        <button className='bg-white hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded shadow-2xl'>Try for free</button>
+        <button className='bg-pink-200 hover:bg-gray-400 text-gray-800 font-bold py-2 px-12 rounded shadow-xl'>Try for free</button>
       </section>
 
       {/* HUNDREDS OF HEALTH PROFESSIONALS */}
@@ -94,18 +94,29 @@ export default function Home() {
       <section className=' border px-4 py-8 border-green-900 w-full '>
 
         <div className='flex flex-col items-center justify-center border border-gray-300 m md:flex-row sm:p-14 '>
+          
           <ul className='border border-gray-300 w-[90%] p-4 m-2.5  md:w-[30%]'>
-            <li><Link href="/about">Help Center</Link></li>
-            <li><Link href="/about">Help Center</Link></li>
-            <li><Link href="/about">Help Center</Link></li>
-            <li><Link href="/about">Help Center</Link></li>
+          <div>Product</div>
+            <li><Link href="/help">Help Center</Link></li>
+            <li><Link href="/business">Flo for Business</Link></li>
+            <li><Link href="/security">Security at Flo</Link></li>
+            <li><Link href="/privacy">Privacy Portal</Link></li>
           </ul>
-          <div className='border border-gray-300 w-[90%] p-4 m-2.5  md:w-[30%]'>
-            Accurate predictions Tracking your periods and ovulation with Flo can help you calculate and predict symptoms ahead of time, getting y ou prepared for the days ahead.
-          </div>
-          <div className='border border-gray-300 w-[90%] p-4 m-2.5  md:w-[30%]'>
-            Accurate predictions Tracking your periods and ovulation with Flo can help you calculate and predict symptoms ahead of time, getting y ou prepared for the days ahead.
-          </div>
+          <ul className='border border-gray-300 w-[90%] p-4 m-2.5  md:w-[30%]'>
+          <div>Company</div>
+            <li><Link href="/about">About Us</Link></li>
+            <li><Link href="/careers">Careers</Link></li>
+            <li><Link href="/contact">Contact Us</Link></li>
+            <li><Link href="/press">Press Center</Link></li>
+            <li><Link href="/body">Your Body your story</Link></li>
+          </ul>
+          <ul className='border border-gray-300 w-[90%] p-4 m-2.5  md:w-[30%]'>
+          <div>Consent</div>
+            <li><Link href="/health">Health Library</Link></li>
+            <li><Link href="/editorial">Editorial Process and Standards</Link></li>
+            <li><Link href="/advertisng">Advertising Principles</Link></li>
+            <li><Link href="/tools">Tools</Link></li>
+          </ul>
         </div>
 
       </section>
