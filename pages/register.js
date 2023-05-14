e3fd49687807d03a5363177921fef3662df538ba
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2023-05-14 02:22:53 +0100

    divided them into sections

--- pages/register.js
+++ pages/register.js
@@ -2,25 +2,48 @@ import { Inter, Roboto_Mono } from 'next/font/google';
 import Header from '@/components/header'
 
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-inter',
 });
- 
+
 const roboto_mono = Roboto_Mono({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-roboto-mono',
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-roboto-mono',
 });
 
 function register() {
-  return (
-     <main className={`min-h-screen   font-mono font-bold border border-red-800 mt-20 `}>
-        <Header />
+    return (
+        <>
+            <Header />
+            <main className={`min-h-screen flex flex-col justify-center font-bold border border-red-800 md:mt-20 `}>
 
 
-     </main>
-  )
+                <div className='flex flex-col items-center h w-[40%] bg-blue-200 m-2.5  '>
+                    <h3 className='mt-3'>Create a New Account</h3>
+
+                    <div className='flex flex-col border border-green-800 p-12'>
+                        <div className='flex flex-col border border-red-800'>
+                            <input type='' placeholder='Name' />
+                        </div>
+
+                        <div className='flex flex-col border border-red-800'>
+                            <input type='' placeholder='Phone No' />
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='flex flex-col w-[40%] m-2.5 border border-gray-300'>
+                    <div className=''>1st box</div>
+                    <div className=''>2nd box</div>
+                </div>
+
+
+            </main>
+        </>
+    )
 }
 
 export default register
