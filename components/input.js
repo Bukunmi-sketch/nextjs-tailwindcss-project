3b1a59e8e9762c0e3476cee54a9825be21100cb8
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2023-05-18 07:44:11 +0100

    created a sendpost function

--- components/input.js
+++ components/input.js
@@ -3,22 +3,48 @@ import nurse from "@/assets/nurse.png";
 import { useRef, useState } from "react";
 import { XMarkIcon, PhotoIcon, ChartBarIcon, CalendarIcon, FaceSmileIcon } from '@heroicons/react/24/outline'
 
-import "emoji-mart/css/emoji-mart.css";
+//import "emoji-mart/css/emoji-mart.css";
 import data from '@emoji-mart/data'
 import Picker from '@emoji-mart/react'
 
 function Input() {
 
     const [input, setInput] = useState("");
-    const [selectedfile, setSelectedFile] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
     const [showEmojis, setShowEmojis] = useState(false);
+    const [loading,setLoading] =useState(false);
     const filePickerRef = useRef(null);
 
-    const addImageToPost=()=>{
 
+    const  addEmoji =(e)=>{
+            let sym= e.unified.split("_");
+            let codesArray =[];
+            sym.forEach((el) => codesArray.push("0x" + el));
+            let emoji = String.fromCodePoint(...codesArray);
+            setInput(input + emoji);
+    }
+
+    const sendPost=()=>{
+        if(loading) return;
+        setLoading(true);
+
+        if(selectedFile){
+            await
+        }
+
+        setLoading(false);
+        setInput("");
+        setSelectedFile(null);
+        setShowEmojis(false);
     }
 
 
+
+    const addImageToPost=()=>{
+           
+    }
+
+   
     return (
         <div
             className={`border-b border-gray-700 flex space-x-3 overflow-y-scroll`}
@@ -29,7 +55,7 @@ function Input() {
                 className="rounded-full h-11 w-11 cursor-pointer shadow-xl"
             />
             <div className="w-full divide-y divide-gray-700">
-                <div>
+                <div className={`${selectedFile && "pb-7"} ${input && "space-y-2.5"}`}>
                     <textarea
                         className="bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]"
                         placeholder="what's happening"
@@ -39,13 +65,13 @@ function Input() {
                     />
                 </div>
 
-                {selectedfile && (
+                {selectedFile && (
                     <div className="relative">
                         <div className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#277c26]bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer " onClick={() =>setSelectedFile(null)}>
                             <XMarkIcon className="text-white h-5" />
                         </div>
 
-                        <Image src={selectedfile} alt="" className="rounded-2xl max-h-80 object-contain" />
+                        <Image src={selectedFile} alt="" className="rounded-2xl max-h-80 object-contain" />
                     </div>
                 )}
 
@@ -68,9 +94,14 @@ function Input() {
                             <CalendarIcon className="h-[22px] text-[#1d9bf0]"/>
                         </div>
 
-                        {/* {} */}
+                       {showEmojis && (
+                         <Picker data={data} onEmojiSelect={addEmoji} 
+                         style={ { position:"absolute",marginTop: "465px", marginLeft: -40, maxWidth:"320px", borderRadius:"20px" }} theme="dark" />
+                       )
+                       }
 
                     </div>
+                    <button className="bg-[#169bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50  disabled:cursor-pointer " disabled={!input.trim() && !selectedFile} onClick={sendPost}>Tweet</button>
                 </div>
 
 
